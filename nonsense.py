b819aa4d0e065c6d9675d84bc211e71bdf901bcf
Markus Amalthea Magnuson <markus.magnuson@gmail.com>
2014-02-25 00:33:48 +0100

    Abstract db interaction through SQLAlchemy, add support for connection string.

--- nonsense.py
+++ nonsense.py
@@ -4,7 +4,7 @@
 # Reads input and generates nonsense based on it.
 #
 # Usage:
-# ./nonsense.py [--lookback N] [--startword Hello] [--min-length 20] [--max-length 300] [--no-cache] [--only-cache] input_file_with_lotsa_words.txt
+# ./nonsense.py [--lookback N] [--startword Hello] [--min-length 20] [--max-length 300] [--no-cache] [--only-cache] [--db-connect-string dialect+driver://username:password@host:port/database] input_file_with_lotsa_words.txt
 #
 # Has quite good performance compared to last version. With the swedish bible
 # it needs about 20MB RAM and can create new sentences in ~0.1s with a prebuilt cache.
@@ -13,63 +13,99 @@
 #
 # Do what you want with it, but please give credit and contribute improvements!
 
-import sqlite3
 import sys
 import random
 import datetime
 import re
 
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, Float, Index
+from sqlalchemy.engine.reflection import Inspector
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import select, update
+from sqlalchemy.sql.expression import func
+
+
+# Set to True to get a lot of useful debug output from sqlalchemy
+VERBOSE = False
+
 WRITE_TO_DB_AFTER_NUM_WORDS = 5000
+
+TABLE_NAME = 'markov_chain'
+TEMP_TABLE_NAME = '%s_temp' % TABLE_NAME
+
 
 def stderr(str):
     sys.stderr.write(str + "\n")
 
+
 def simple_time_diff(str, d1, d2):
-    stderr("Execution for %s took %d seconds" % (str, (d2-d1).seconds))
+    seconds = (d2-d1).seconds
+    stderr("Execution for %s took %d second%s" % (str, seconds, ('s' if seconds > 1 else '')))
+
 
 class MarkovChain(object):
-    def __init__(self, input_file=None, lookback=3, no_cache=False):
+    def __init__(self, input_file=None, lookback=3, no_cache=False, db_connect_string=None):
         self.WORD_RE = re.compile(r"([\w\.\!\,]+)", re.UNICODE)
         self.NO_REAL_WORD_RE = re.compile(r"^[\d\.\,\:\;]*$")
         self.lookback = lookback
 
-        if no_cache:
-            self.conn = sqlite3.connect(":memory:")
+        if db_connect_string:
+            self.engine = create_engine(db_connect_string, echo=VERBOSE)
         else:
-            self.conn = sqlite3.connect("%s.markovdb~%d" % (input_file, lookback))
+            if no_cache:
+                self.engine = create_engine('sqlite://', echo=VERBOSE)  # :memory:
+            else:
+                self.engine = create_engine('sqlite:///%s.markovdb~%d' % (input_file, lookback), echo=VERBOSE)
 
-        self.c = self.conn.cursor()
+        Session = sessionmaker(bind=self.engine)
+        self.session = Session()
 
         if input_file:
             self.input(input_file)
 
     def input(self, input_file):
-        # check if cached db already exists
-        if len(self.c.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='markov_chain'").fetchall()) < 1:
-            # re-generate everything from scratch.
-            self.init_database()
+        metadata = MetaData()
+        metadata.bind = self.engine
 
+        # Define the main table.
+        self.table = Table(TABLE_NAME, metadata,
+                           Column('prefix', String),
+                           Column('suffix', String),
+                           Column('num_occurrences', Integer, default=0),
+                           Column('probability', Float))
+        Index('prefix_index', self.table.c.prefix)
+
+        if self.table.exists() and self.table.count().scalar() > 0:
+            stderr('Table %s already exists, will reuse its data.' % TABLE_NAME)
+        else:
+            self.table.drop(checkfirst=True)
+            metadata.create_all()
             with open(input_file) as file:
                 self.generate_markov_chain(file.read().decode("utf-8"))
 
-    def init_database(self):
-       self.c.execute("CREATE TABLE IF NOT EXISTS markov_chain (prefix text, suffix text, num_occurrences integer DEFAULT 0, probability real)");
-       self.c.execute("CREATE INDEX prefix_index ON markov_chain (prefix)")
-
     def generate_markov_chain(self, input):
+        # create "temporary" table for initial data batch
+        metadata = MetaData()
+        temptable = Table('%s' % TEMP_TABLE_NAME, metadata,
+                          Column('prefix', String),
+                          Column('suffix', String))
+        Index('prefix_suffix_index', temptable.c.prefix, temptable.c.suffix)
+        temptable.drop(self.engine, checkfirst=True)
+        metadata.create_all(self.engine)
+
         # interface to the db
         def save_suffixes(suffixes):
-            self.c.executemany("INSERT INTO markov_chain_temp (prefix, suffix) VALUES (?, ?)", suffixes)
+            if suffixes:
+                self.session.execute(temptable.insert(), [{
+                    'prefix': prefix,
+                    'suffix': suffix,
+                } for prefix, suffix in suffixes])
+                self.session.commit()
 
         d1 = datetime.datetime.now()
 
-        # create "temporary" table for initial data batch
-        self.c.execute("DROP TABLE IF EXISTS markov_chain_temp")
-        self.c.execute("CREATE TABLE markov_chain_temp (prefix text, suffix text)")
-        self.c.execute("CREATE INDEX prefix_suffix_index ON markov_chain_temp (prefix, suffix)")
-
         # estimate num words so we can give progress
-        word_count_estimate = input.count(' ') + 1
+        word_count_estimate = input.count(" ") + 1
 
         prev_prefixes = []
         unsaved_suffixes = []
@@ -102,9 +138,6 @@ class MarkovChain(object):
 
         # final save of suffixes to database
         save_suffixes(unsaved_suffixes)
-        unsaved_suffixes = []
-
-        self.conn.commit()
 
         d2 = datetime.datetime.now()
         simple_time_diff("Word indexing", d1, d2)
@@ -114,12 +147,23 @@ class MarkovChain(object):
         # insert all rows from the temporary table into the final table, but
         # collapsed into one row per prefix+suffix combo, with its count in the
         # as num_occurrences in the final table
-        self.c.execute("INSERT INTO markov_chain (prefix, suffix, num_occurrences)\
-            SELECT prefix, suffix, count(*) FROM markov_chain_temp GROUP BY prefix, suffix")
+        rows = self.session.execute(select([
+            temptable.c.prefix,
+            temptable.c.suffix,
+            func.count()
+        ]).group_by(temptable.c.prefix, temptable.c.suffix)).fetchall()
 
-        self.c.execute("DROP TABLE markov_chain_temp")
+        self.session.execute(self.table.insert(), [{
+            'prefix': row[0],
+            'suffix': row[1],
+            'num_occurrences': row[2]
+        } for row in rows])
 
-        self.conn.commit()
+        self.session.commit()
+
+        temptable.drop(self.engine, checkfirst=True)
+
+        self.session.commit()
 
         d3 = datetime.datetime.now()
         simple_time_diff("Word counting", d2, d3)
@@ -127,13 +171,21 @@ class MarkovChain(object):
         stderr("Calculating probabilities...")
 
         # re-count from num occurences of each suffix => probability (from 0.0 - 1.0)
-        for row in self.c.execute("SELECT prefix, sum(num_occurrences) FROM markov_chain GROUP BY prefix").fetchall():
-            self.c.execute("UPDATE markov_chain SET probability=((1.0*num_occurrences)/?) WHERE prefix=?", (float(row[1]), row[0]))
+        rows = self.session.execute(select([self.table.c.prefix, func.sum(self.table.c.num_occurrences)]).group_by(self.table.c.prefix)).fetchall()
+        for row in rows:
+            self.session.execute(update(self.table).values(
+                probability=(1.0*self.table.c.num_occurrences)/float(row[1])
+            ).where(self.table.c.prefix == row[0]))
 
-        self.conn.commit()
+        self.session.commit()
 
         # vacuum database to keep disk usage to a minimum
-        self.c.execute("VACUUM")
+        # TODO: This is not database independent; can we check for vacuum support?
+        #self.session.execute("VACUUM")
+        # TODO: Fix error "VACUUM cannot run inside a transaction block" on postgres
+
+        self.session.commit()
+
         d4 = datetime.datetime.now()
 
         simple_time_diff("Probabilities and vacuuming", d3, d4)
@@ -141,10 +193,13 @@ class MarkovChain(object):
     def choose_next_word(self, from_prefix):
         random_choice = random.random()
         i = 0
-        for row in self.c.execute("SELECT suffix, probability FROM markov_chain WHERE prefix=? ORDER BY RANDOM()", (from_prefix,)):
-           i += row[1]
-           if i >= random_choice:
-               return row[0]
+        for row in self.session.execute(select([
+            self.table.c.suffix,
+            self.table.c.probability
+        ]).where(self.table.c.prefix == from_prefix).order_by(func.random())):  # TODO: 'random()' is not database independent
+            i += row[1]
+            if i >= random_choice:
+                return row[0]
 
     def generate_sentence(self, start_word=None, min_words=5, max_length=140, prevent_recursion=False):
         first_word = None
@@ -186,7 +241,7 @@ class MarkovChain(object):
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        stderr("Usage: ./nonsense.py [--lookback N] [--startword Hello] [--min-length 20] [--max-length 300] [--no-cache] [--only-cache] input_file_with_lotsa_words.txt")
+        stderr("Usage: ./nonsense.py [--lookback N] [--startword Hello] [--min-length 20] [--max-length 300] [--no-cache] [--only-cache] [--db-connect-string dialect+driver://username:password@host:port/database] input_file_with_lotsa_words.txt")
         sys.exit(1)
 
     # default arguments
@@ -195,6 +250,7 @@ if __name__ == "__main__":
     lookback = 3
     no_cache = False
     only_cache = False
+    db_connect_string = None
 
     # ugly but simple enough argument handling
     args = sys.argv[1:]
@@ -202,6 +258,12 @@ if __name__ == "__main__":
     if "--startword" in args:
         i = args.index("--startword")
         start_word = args[i+1].decode("utf-8")
+        del args[i+1]
+        del args[i]
+    if "--min-length" in args:
+        raise Exception("--min-length is not implemented yet!")
+        i = args.index("--min-length")
+        min_length = args[i+1]
         del args[i+1]
         del args[i]
     if "--max-length" in args:
@@ -221,12 +283,17 @@ if __name__ == "__main__":
         i = args.index("--only-cache")
         only_cache = True
         del args[i]
+    if "--db-connect-string" in args:
+        i = args.index("--db-connect-string")
+        db_connect_string = args[i+1]
+        del args[i+1]
+        del args[i]
 
     input_sources = args
     source = args[0]
 
     stderr("Generating Markov chain for input %s..." % source)
-    markov_chain = MarkovChain(input_file=args[0], lookback=lookback, no_cache=no_cache)
+    markov_chain = MarkovChain(input_file=args[0], lookback=lookback, no_cache=no_cache, db_connect_string=db_connect_string)
 
     if only_cache:
         stderr("Told to only generate a cache, so exiting now.")
